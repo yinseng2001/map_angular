65b5a451e352c96281dc522fb7106452721bc5eb
yinseng2001 <yin_seng2001@yahoo.com>
2016-10-14 15:36:00 +0700

    fixed lip

--- js/controller.js
+++ js/controller.js
@@ -2,73 +2,12 @@ App.controller("MainController", function ($scope) {
 
 });
 
-<<<<<<< HEAD
 App.controller("HomeController", function ($scope, $location) {
-=======
-App.controller("HomeController",function ($scope,$location){
-	
-	$scope.openMap = function(){
-		$location.path("/map");
-	}
-});
-
-App.controller("MapController", function ($scope,$location, $localStorage,$geolocation,NgMap) {
-	
-	var cMap = this;
-	var marker;
-        
-
-if (navigator.geolocation) {
-NgMap.getMap().then(function(map) {
-	    
-	    //Store current map
-	    cMap = map;
-	    $scope.curPos = {};
-
-	    marker = map.markers[0];
-
-	    //Request geolocation
-	    
-
-	    
-	  });
-			$geolocation.getCurrentPosition({
-			    timeout: 60000
-			}).then(function(position) {
-			});
-		}else{
-			alert("Browser doesn't support Geolocation");
-		}
-
-	
-	
-
-
-	$scope.getCenter = function(){
-
-		$localStorage.savedPos = {
-			'lat' : cMap.getCenter().lat(),
-			'lng' : cMap.getCenter().lng()
-		}
-
-		$location.path("/");
-
-	};
-
-	$scope.gotoPos = function(){
-		cMap.panTo(marker.getPosition());
-	};
-
-	$scope.goBack = function(){
-		$location.path("/");
-	}
->>>>>>> 238c20ae87958b68a1b0f11be224763803f01318
 
     $scope.openMap = function () {
         $location.path("/map");
     }
 });
-<<<<<<< HEAD
 
 App.controller("MapController", function ($scope, $location, $localStorage, $geolocation, uiGmapIsReady) {
 
@@ -151,6 +90,3 @@ App.controller("MapController", function ($scope, $location, $localStorage, $geo
     }
 
 });
-=======
-	
->>>>>>> 238c20ae87958b68a1b0f11be224763803f01318
