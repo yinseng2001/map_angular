c827b2a13b84c8b1da094c956ee74de383be5800
yinseng2001 <yin_seng2001@yahoo.com>
2016-10-14 15:29:11 +0700

    fix lib

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 //Initialize
-var App = angular.module("App", ['ngRoute','routeStyles','ngMap','ngGeolocation','ngStorage']);
+var App = angular.module("App", ['uiGmapgoogle-maps','ngRoute','routeStyles','ngGeolocation','ngStorage']);
 
 //Configuration
 App.config(['$compileProvider', function ($compileProvider) {
     $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob):|data:image\//);
     $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|blob):|data:image/);
 }]);
+
+App.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyAb-d7K-xK4f8QnT-EONHvTejsYx2ZlJtM',
+        v: '3.20',
+    });
+});
 
 //Route
 App.config(function ($routeProvider) {
@@ -21,8 +28,11 @@ App.config(function ($routeProvider) {
 });
 
 //Bootstrap
-App.run(function ($rootScope) {
+App.run(function ($rootScope,$templateCache) {
     
+    $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
     
 
 });
--- js/controller.js
+++ js/controller.js
@@ -1,60 +1,92 @@
-
 App.controller("MainController", function ($scope) {
 
 });
 
-App.controller("HomeController",function ($scope,$location){
-	
-	$scope.openMap = function(){
-		$location.path("/map");
-	}
+App.controller("HomeController", function ($scope, $location) {
+
+    $scope.openMap = function () {
+        $location.path("/map");
+    }
 });
 
-App.controller("MapController", function ($scope,$location, $localStorage,$geolocation,NgMap) {
-	
-	var cMap = this;
-	var marker;
+App.controller("MapController", function ($scope, $location, $localStorage, $geolocation, uiGmapIsReady) {
 
-	
-	NgMap.getMap().then(function(map) {
-	    
-	    //Store current map
-	    cMap = map;
-	    $scope.curPos = {};
+    $(".container").css("height", $(window).innerHeight());
+    $(".container").css("width", $(window).innerWidth());
 
-	    marker = map.markers[0];
+    $(".angular-google-map-container").css("height", $(window).innerHeight());
+    $(".angular-google-map-container").css("width", $(window).innerWidth());
 
-	    //Request geolocation
-	    if (navigator.geolocation) {
-			$geolocation.getCurrentPosition({
-			    timeout: 60000
-			}).then(function(position) {
-			});
-		}else{
-			alert("Browser doesn't support Geolocation");
-		}
+    var map;
+    var uuid;
+    var mapInstanceNumber;
 
-	    
-	  });
+    $scope.map = {
+        center: {
+            latitude: 11.560827699999999,
+            longitude: 104.92362909999997
+        },
+        zoom: 21,
+        options: {
+            mapTypeId: "satellite"
+        }
+    };
+
+    $scope.marker = {
+        id: 1,
+        options: {
+            icon: "images/cPos.png"
+        },
+        coords: {}
+    };
+
+    //Get current location
+    if (navigator.geolocation) {
+        $geolocation.getCurrentPosition({
+            timeout: 60000
+        }).then(function (position) {
+            $scope.myPosition = position;
+            $scope.map.center.latitude = $scope.myPosition.coords.latitude;
+            $scope.map.center.longitude = $scope.myPosition.coords.longitude;
+
+            $scope.marker.coords = {
+                latitude: $scope.myPosition.coords.latitude,
+                longitude: $scope.myPosition.coords.longitude
+            };
+        });
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 
 
-	$scope.getCenter = function(){
 
-		$localStorage.savedPos = {
-			'lat' : cMap.getCenter().lat(),
-			'lng' : cMap.getCenter().lng()
-		}
+    uiGmapIsReady.promise(1).then(function (instances) {
+        instances.forEach(function (inst) {
+            map = inst.map;
+            uuid = map.uiGmap_id;
+            mapInstanceNumber = inst.instance;
+        });
+    });
 
-		$location.path("/");
 
-	};
+    $scope.getCenter = function () {
 
-	$scope.gotoPos = function(){
-		cMap.panTo(marker.getPosition());
-	};
+        $localStorage.savedPos = {
+            'lat': map.getCenter().lat(),
+            'lng': map.getCenter().lng()
+        }
 
-	$scope.goBack = function(){
-		$location.path("/");
-	}
+        $location.path("/");
+
+    };
+
+    $scope.gotoPos = function () {
+        var point = new google.maps.LatLng($scope.marker.coords.latitude, $scope.marker.coords.longitude);
+        map.panTo(point);
+    };
+
+    $scope.goBack = function () {
+        $location.path("/");
+    }
 
 });
